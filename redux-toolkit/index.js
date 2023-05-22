d131aec90a188d3526095a07d637d684624850ba
Deepesh Bhardwaj <70226498+deepesh16b@users.noreply.github.com>
2023-05-22 19:10:23 +0000

    async thunk action creator done

--- redux-toolkit/index.js
+++ redux-toolkit/index.js
@@ -1,16 +1,18 @@
 const store = require("./app/store");
+const { fetchUsers } = require("./features/user/userSlice");
 
 const cakeActions = require("./features/cake/cakeSlice").cakeActions;
-const icecreamActions = require("./features/icecream/icecreamSlice").icecreamActions;
+const icecreamActions =
+  require("./features/icecream/icecreamSlice").icecreamActions;
 
 const unsubscribe = store.subscribe(() => {
-//   console.log("updated state:>>  ", store.getState());
+  console.log("updated state:>>  ", store.getState());
 });
 
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.restocked(5));
-store.dispatch(icecreamActions.ordered());
-store.dispatch(icecreamActions.restocked(5));
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.restocked(5));
+// store.dispatch(icecreamActions.ordered());
+// store.dispatch(icecreamActions.restocked(5));
 
-
-unsubscribe();
+// ----fetchUsers is an action Creator made using asyncThunk
+store.dispatch(fetchUsers());
