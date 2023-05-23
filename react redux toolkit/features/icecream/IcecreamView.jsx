492ac3bad541166d0b1c2fdcdb67cff2b2ba5aa0
Deepesh Bhardwaj <70226498+deepesh16b@users.noreply.github.com>
2023-05-23 22:54:00 +0530

    Delete react redux toolkit directory

--- react redux toolkit/features/icecream/IcecreamView.jsx
+++
@@ -1,9 +0,0 @@
-// import React from "react";
-
-export const IcecreamsView = () => {
-  return <div>
-    <h2>Number of Icecreams - </h2>
-    <button>Order Icecreams</button>
-    <button>restock Icecreams</button>
-  </div>;
-};
--- react redux toolkit/features/user/UserView.jsx
+++
@@ -1,9 +0,0 @@
-// import React from "react";
-
-export const UserView = () => {
-  return (
-    <div>
-      <h2>List of users - </h2>
-    </div>
-  );
-};
